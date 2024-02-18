e4dbab63b24c31cb2249f47bd88deb96fa3e79e8
Dani Perianto <daniperianto007@gmail.com>
2024-02-18 22:01:32 +0700

    first commit

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <x-card class="p-10 max-w-lg mx-auto mt-24">
         <header class="text-center">
-            <h2 class="text-2xl font-bold uppercase mb-1">Create a Gig</h2>
-            <p class="mb-4">Post a gig to find a developer</p>
+            <h2 class="text-2xl font-bold uppercase mb-1">Create a Job</h2>
+            <p class="mb-4">Post a job</p>
         </header>
 
         <form method="POST" action="/listings" enctype="multipart/form-data">
